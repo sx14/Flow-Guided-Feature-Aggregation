3f6179c1a61dd911bda2739b07894b45ed5b86d7
sunx <1059363093@qq.com>
2020-02-27 09:57:09 +0800

    fix env var: PYTHONPATH in sh files

--- train_vidor.sh
+++ train_vidor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-export PYTHONPATH=/home/magus/sunx-workspace/project/Flow-Guided-Feature-Aggregation:$PYTHONPATH
+export PYTHONPATH=$(pwd):$PYTHONPATH
 python experiments/fgfa_rfcn/fgfa_rfcn_end2end_train_test.py --cfg experiments/fgfa_rfcn/cfgs/resnet_v1_101_flownet_vidor_vid_rfcn_end2end_ohem.yaml
